161c8082a2d34d6ebfb6cc19f30b82ac6eb93b4c
ricky <rjbenavides@cpp.edu>
2020-07-05 18:11:13 -0700

    Added custom hook useForm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useForm } from './useForm';
 
 
-function App() {
-  return (
-    <div className="App">
-     hello
-    </div>
-  );
+
+const App = () => {
+ const [{count, count2},setCount] =useState({count:10, count2: 20})
+ const [email,setEmail] = useState('');
+ const [password, setPassword] = useState('');
+ const [values, handleChange]= useForm({email:'' ,password:''})
+ return (
+   <div>
+     <button onClick={()=>
+     setCount(currentState => ({...currentState,count :currentState.count + 1
+     }))
+     }>
+      +
+     </button>
+     <input
+      name='email'
+      value={values.email}
+      onChange={handleChange}
+
+     />
+     <input type='password' name='password' value={values.password} onChange={handleChange}/>
+   </div>
+ )
 }
 
 export default App;
